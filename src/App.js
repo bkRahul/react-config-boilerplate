89ae7077755dcacd5872c425b8601f95e317d722
bkRahul <rahulsatya1068@gmail.com>
2019-06-17 01:06:30 +0530

    starter boilerplate built for react using webpack

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.scss";
+
+const App = () => {
+  return (
+    <div className="full-screen">
+      <div>
+        <h1>
+          React Config Boilerplate
+        </h1>
+        <br />
+      </div>
+    </div>
+  );
+};
+
+export default App;
